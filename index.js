0fc2671d747c685747d8d823d7a91bc1de0f8e7e
David Lozano <dawgun@hotmail.com>
2022-07-12 20:36:57 +0200

    Added function in index.js and created index.html

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-import strictEquals from "./modules-js/strictEquals.js";
-
-console.log(strictEquals());
+const strictEquals = function (firstValue, secondValue) {
+  if (Object.is(firstValue, secondValue)) {
+    if (Number.isNaN(firstValue) && Number.isNaN(secondValue)) {
+      return false;
+    }
+    return true;
+  } else {
+    let firstZero = [firstValue];
+    let secondZero = [secondValue];
+    if (
+      (firstZero.includes(0) && secondZero.includes(-0)) ||
+      (firstZero.includes(-0) && secondZero.includes(0))
+    ) {
+      return true;
+    }
+    return false;
+  }
+};
--- modules-js/strictEquals.js
+++ modules-js/strictEquals.js
@@ -1,20 +1,2 @@
-const strictEquals = function (firstValue, secondValue) {
-  if (Object.is(firstValue, secondValue)) {
-    if (Number.isNaN(firstValue) && Number.isNaN(secondValue)) {
-      return false;
-    }
-    return true;
-  } else {
-    let firstZero = [firstValue];
-    let secondZero = [secondValue];
-    if (
-      (firstZero.includes(0) && secondZero.includes(-0)) ||
-      (firstZero.includes(-0) && secondZero.includes(0))
-    ) {
-      return true;
-    }
-    return false;
-  }
-};
 
-export default strictEquals;
+
